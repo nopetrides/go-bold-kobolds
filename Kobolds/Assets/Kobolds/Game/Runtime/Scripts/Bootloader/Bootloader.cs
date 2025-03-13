d657500557ed0912f7e811f868e8195f5a234963
nopetrides <nopetrides@gmail.com>
2025-03-13 04:35:28 -0700

    updated intro, fix language

--- Kobolds/Assets/Kobolds/Game/Runtime/Scripts/Bootloader/Bootloader.cs
+++ Kobolds/Assets/Kobolds/Game/Runtime/Scripts/Bootloader/Bootloader.cs
@@ -1,18 +1,70 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using Febucci.UI;
 using Kobolds.Runtime.Managers;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Kobolds.Runtime
 {
     public class Bootloader : MonoBehaviour
     {
-        [SerializeField] private Animator AnimatorComponent;
+		[FormerlySerializedAs("_gameLoader")] 
+		[SerializeField] private GlobalsMgr GameLoader;
 
-		[SerializeField] private GlobalsMgr _gameLoader;
+		[SerializeField] private float InDelay = 0.1f;
+		[SerializeField] private float OutDelay = 0.1f;
+		
+		[SerializeField] private TypewriterByCharacter StartingTypewriter;
 
-		// Called by the animator after finishing the splash intro
+		Sequence _sequence;
+		
+		private void OnEnable()
+		{
+			StartSequence();
+		}
+
+		private void StartSequence()
+		{
+			_sequence = DOTween.Sequence();
+			_sequence.SetDelay(InDelay);
+			_sequence.OnComplete(ShowSplash);
+			_sequence.Play();
+		}
+
+		private void ShowSplash()
+		{
+			StartingTypewriter.ShowText("P");
+		}
+
+
+		// Called by the splash after finishing
         public void LoadIntro()
 		{
-			Instantiate(_gameLoader);
+			EndSequence();
 		}
-    }
+
+		private void EndSequence()
+		{
+			_sequence = DOTween.Sequence();
+			_sequence.SetDelay(OutDelay);
+			_sequence.OnComplete(IntroComplete);
+			_sequence.Play();
+		}
+
+		private void IntroComplete()
+		{
+			Instantiate(GameLoader);
+		}
+
+		public void Update()
+		{
+			if (Input.anyKeyDown)
+			{
+				_sequence.Kill();
+				IntroComplete();
+			}
+		}
+	}
 }
